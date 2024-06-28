81c5e4241ef7b354877edfe810954c7906551980
Emilio Pelaez Cisneros <epelaez@uchicago.edu>
2024-06-28 13:22:49 -0500

    Add double differential vars to selection efficiency plots

--- CAFAnaAnalysis/Scripts/SelectionEfficiency.C
+++ CAFAnaAnalysis/Scripts/SelectionEfficiency.C
@@ -10,10 +10,10 @@
 #include "TLegend.h"
 #include "TLegendEntry.h"
 #include "TEfficiency.h"
-#include "TGaxis.h"
 #include "TGraphAsymmErrors.h"
 #include "TFile.h"
 #include "TH1D.h"
+#include "TLatex.h"
 
 // std includes.
 #include <vector>
@@ -117,6 +117,26 @@ void SelectionEfficiency() {
     Vars.push_back(kTruthRecoilProtonMomentum); VarBins.push_back(bProtonMomentumBins);
     PlotNames.push_back("TruthRecoilProtonMomentum"); VarLabels.push_back("|#vec{p}_{R}|");
 
+    ////////////////////////////////
+    // Double differential variables
+    ////////////////////////////////
+
+    // Serial transverse momentum in muon cos theta
+    Vars.push_back(kTruthTransverseMomentumInMuonCosTheta); VarBins.push_back(bTransverseMomentumInMuonCosTheta);
+    PlotNames.push_back("TrueSerialTransverseMomentum_InMuonCosTheta"); VarLabels.push_back("#delta P_{T} (bin #)");
+
+    // Delta alpha transverse in muon cos theta
+    Vars.push_back(kTruthDeltaAlphaTInMuonCosTheta); VarBins.push_back(bDeltaAlphaTInMuonCosTheta);
+    PlotNames.push_back("TrueSerialDeltaAlphaT_InMuonCosTheta"); VarLabels.push_back("#delta #alpha_{T} (bin #)");
+
+    // Opening angle between protons in muon cos theta
+    Vars.push_back(kTruthCosOpeningAngleProtonsInMuonCosTheta); VarBins.push_back(bCosOpeningAngleProtonsInMuonCosTheta);
+    PlotNames.push_back("TrueSerialCosOpeningAngleProtons_InMuonCosTheta"); VarLabels.push_back("cos(#theta_{#vec{p}_{L},#vec{p}_{R}}) (bin #)");
+    
+    // Opening angle between muon and protons in muon cos theta
+    Vars.push_back(kTruthCosOpeningAngleMuonTotalProtonInMuonCosTheta); VarBins.push_back(bCosOpeningAngleMuonTotalProtonInMuonCosTheta);
+    PlotNames.push_back("TrueSerialCosOpeningAngleMuonTotalProton_InMuonCosTheta"); VarLabels.push_back("cos(#theta_{#vec{p}_{#mu},#vec{p}_{sum}}) (bin #)");
+
     // Construct all spectra
     std::vector<std::tuple<std::unique_ptr<Spectrum>, std::unique_ptr<Spectrum>>> Spectra;
     for (std::size_t i = 0; i < Vars.size(); i++) {
@@ -131,33 +151,99 @@ void SelectionEfficiency() {
         auto& [TrueSignals, RecoTrueSignals] = Spectra.at(i);
 
         TCanvas* PlotCanvas = new TCanvas("Selection","Selection",205,34,1124,768);
-        TH1* TrueHisto = TrueSignals->ToTH1(TargetPOT);
-        TH1* RecoTrueHisto = RecoTrueSignals->ToTH1(TargetPOT);
+        TH1D* TrueHisto = TrueSignals->ToTH1(TargetPOT);
+        TH1D* RecoTrueHisto = RecoTrueSignals->ToTH1(TargetPOT);
 
+        // Change y axis title so efficiency plot inherits it
         TrueHisto->GetYaxis()->SetTitle("Signal efficiency");
         RecoTrueHisto->GetYaxis()->SetTitle("Signal efficiency");
 
-        TEfficiency* Eff = new TEfficiency(*RecoTrueHisto, *TrueHisto);
-
+        // Canvas margins
         PlotCanvas->SetTopMargin(0.13);
         PlotCanvas->SetLeftMargin(0.17);
         PlotCanvas->SetRightMargin(0.05);
         PlotCanvas->SetBottomMargin(0.16);
 
-        PlotCanvas->cd();
-        Eff->SetMarkerStyle(21);
-        Eff->SetMarkerColor(kBlack);
-        Eff->SetTitle("All events");
-        Eff->Draw("AP");
-        gPad->Update();
-        Eff->GetPaintedGraph()->GetXaxis()->SetRangeUser(VarBins.at(i).Min(),VarBins.at(i).Max());
+        if (PlotNames[i].Contains("Serial")) {
+            // Flatten out double differential plots
+            auto [SliceDiscriminators, SliceBinning] = PlotNameToDiscriminator[PlotNames[i]+"Plot"];
+            auto [NSlices, SerialVectorRanges, SerialVectorBins, SerialVectorLowBin, SerialVectorHighBin] = tools.FlattenNDBins(SliceDiscriminators, SliceBinning);
+            int StartIndex = 0;
 
-        // Save as png
-        PlotCanvas->SaveAs(dir+"/Figs/CAFAna/Efficiency/"+PlotNames[i]+".png");
+            // Loop over slices
+            for (int iSlice = 0; iSlice < NSlices; iSlice++) {
+                // Slice name
+                TString SlicePlotName = PlotNames[i] + "_" + TString(std::to_string(iSlice));
 
-        // Save to root file
-        SaveFile->WriteObject(Eff, PlotNames[i]+"_eff");
+                // Get slice width
+                double SliceWidth = SliceDiscriminators[iSlice + 1] - SliceDiscriminators[iSlice]; 
 
+                // Get number of bins
+                int SliceNBins = SerialVectorBins.at(iSlice);
+                std::vector<double> SerialSliceBinning;
+
+                for (int iBin = 0; iBin < SliceNBins + 1; iBin++) {
+                    double value = SerialVectorRanges.at(StartIndex + iBin);
+                    SerialSliceBinning.push_back(value);
+                } // End of the number of bins and the bin ranges declaration
+
+                // Slice true and reco true histos
+                TH1D* SlicedTrueHisto = tools.GetHistoBins(
+                    TrueHisto,
+                    SerialVectorLowBin.at(iSlice),
+                    SerialVectorHighBin.at(iSlice),
+                    SliceWidth,
+                    SerialSliceBinning,
+                    "True"
+                );
+                TH1D* SlicedRecoTrueHisto = tools.GetHistoBins(
+                    RecoTrueHisto,
+                    SerialVectorLowBin.at(iSlice),
+                    SerialVectorHighBin.at(iSlice),
+                    SliceWidth,
+                    SerialSliceBinning,
+                    "RecoTrue"
+                );
+
+                // Create efficiency plot
+                TEfficiency* Eff = new TEfficiency(*SlicedRecoTrueHisto, *SlicedTrueHisto);
+
+                PlotCanvas->cd();
+                Eff->SetMarkerStyle(21);
+                Eff->SetMarkerColor(kBlack);
+                Eff->Draw("AP");
+                gPad->Update();
+                Eff->GetPaintedGraph()->GetXaxis()->SetRangeUser(SerialSliceBinning.at(0),SerialSliceBinning.at(SerialSliceBinning.size() - 1));
+
+                // Slice label
+                TLatex *textSlice = new TLatex();
+                TString SliceLabel = tools.to_string_with_precision(SliceDiscriminators[iSlice], 1) + " < " + PlotNameToSliceLabel[PlotNames[i]+"Plot"] + " < " + tools.to_string_with_precision(SliceDiscriminators[iSlice + 1], 1);
+                textSlice->DrawLatexNDC(0.4,0.92,SliceLabel);
+
+                // Save as png
+                PlotCanvas->SaveAs(dir+"/Figs/CAFAna/Efficiency/"+SlicePlotName+".png");
+
+                // Save to root file
+                SaveFile->WriteObject(Eff, SlicePlotName+"_eff");
+            }
+        } else {
+            // Create efficiency plot
+            TEfficiency* Eff = new TEfficiency(*RecoTrueHisto, *TrueHisto);
+
+            PlotCanvas->cd();
+            Eff->SetMarkerStyle(21);
+            Eff->SetMarkerColor(kBlack);
+            Eff->SetTitle("All events");
+            Eff->Draw("AP");
+            gPad->Update();
+            Eff->GetPaintedGraph()->GetXaxis()->SetRangeUser(VarBins.at(i).Min(),VarBins.at(i).Max());
+
+            // Save as png
+            PlotCanvas->SaveAs(dir+"/Figs/CAFAna/Efficiency/"+PlotNames[i]+".png");
+
+            // Save to root file
+            SaveFile->WriteObject(Eff, PlotNames[i]+"_eff");
+        }
         delete PlotCanvas;
     }
     // Close file
