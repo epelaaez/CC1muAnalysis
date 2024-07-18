9c62c8664e114011511584a2747ee16230905009
Emilio Pelaez Cisneros <epelaez@uchicago.edu>
2024-07-18 14:21:42 -0500

    Add script to run complete CAFAna analysis

---
+++ CAFAnaAnalysis/run_all.sh
@@ -0,0 +1,7 @@
+cafe -bq Scripts/Selection.C
+root -b Scripts/SerialPlotGenerator.cpp
+cafe -bq Scripts/SelectionEfficiency.C
+cafe -bq Scripts/SelectionMigrationMatrix.C
+cafe -bq Scripts/RunAllSystematics.C
+root -b Scripts/StatSystematics.cpp
+root -b Scripts/Unfold.cpp
