cbc9efc9caa748875e9516679a4cb8f6c41e72d5
Emilio Pelaez Cisneros <epelaez@uchicago.edu>
2024-07-19 13:20:34 -0500

    Update run all script

--- CAFAnaAnalysis/run_all.sh
+++ CAFAnaAnalysis/run_all.sh
@@ -1,7 +1,9 @@
 cafe -bq Scripts/Selection.C
 root -b -q Scripts/SerialPlotGenerator.cpp
+cafe -bq Scripts/SelectionRunData.C
 cafe -bq Scripts/SelectionEfficiency.C
 cafe -bq Scripts/SelectionMigrationMatrix.C
 cafe -bq Scripts/RunAllSystematics.C
 root -b -q Scripts/StatSystematics.cpp
+root -b -q Scripts/TotalCovMatrices.cpp
 root -b -q Scripts/Unfold.cpp
